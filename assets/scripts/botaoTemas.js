bf530ad5de069451d788a53d6e6c23b339260abb
Thalia Danielle <31187727+thaliadani@users.noreply.github.com>
2026-05-28 18:09:11 -0300

    Excluir botaoTemas.js

--- assets/scripts/botaoTemas.js
+++
@@ -1,6 +0,0 @@
-const botaoTemas = document.getElementById('botao-temas');
-const menuTemas = document.getElementById('menu-temas');
-
-botaoTemas?.addEventListener('click', () => {
-    menuTemas.classList.toggle('aberto');
-});
